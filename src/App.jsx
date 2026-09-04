12914df6e669cb4d6119c3558b6b17a91ed391c5
Dabazuka <86969901+Dabazuka@users.noreply.github.com>
2026-09-05 02:24:47 +0530

    Link Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-const API_URL = 'https://campus-360-uvx3.onrender.com/';
+const API_URL = 'https://campus-360-uvx3.onrender.com';
 import {
   GraduationCap,
   Calendar,
